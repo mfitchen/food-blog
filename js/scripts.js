59580beb481ca1e0367f8d9c9d8e84f600f7abe0
Marty Fitchen and Sky Rousse <marty+sky@>
2016-05-31 16:32:28 -0700

    add all new files and directories and edited buttons to change bg and text color

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$("button#dark").click(function() {
+  $("body").removeClass();
+  $("body").addClass("background-dark");
+});
+
+$("button#light").click(function() {
+  $("body").removeClass();
+  $("body").addClass("background-light");
+});
